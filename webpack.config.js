47982afb183f7b2101b33b6fe5024c00f7d00709
entrptaher@gmail.com <entrptaher@gmail.com>
2019-05-14 21:20:05 +0600

    Upgrade versions

--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,32 @@ var APP_DIR = path.resolve(__dirname, "app");
 var BUILD_DIR = path.resolve(__dirname, "build");
 
 var config = {
-	entry: {
-		"dom": APP_DIR + "/index.js",
-	},
-	output: {
-		path: BUILD_DIR,
-		filename: "[name].js"
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			regeneratorRuntime: "regenerator-runtime/runtime"
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["es2015", "stage-0", "react"]
-					}
-				}
-			}
-		]
-	}
+  entry: {
+    dom: APP_DIR + "/index.js"
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      regeneratorRuntime: "@babel/plugin-transform-runtime"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = config;
